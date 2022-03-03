26aa8dd5b6d7541db0e4abddc36f73d6cb8befa3
Sebastian Andres Gallardo <sebastian.gallardo1@mail.udp.cl>
2022-03-03 19:47:25 -0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { JournalApp } from './JournalApp';
-import { AppRouter } from './routers/AppRouter';
+
 import './styles/styles.scss'
 
 
 ReactDOM.render(
-  <AppRouter>
-     <JournalApp />
-  </AppRouter>,
+     <JournalApp />,
   document.getElementById('root')
 );
